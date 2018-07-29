ce85768c96fcf8648fb229efc55a7995445a13f2
Caroline Even <carolineven29@gmail.com>
2018-07-29 17:50:00 +1000

    Basic homepage

---
+++ src/Homepage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './Homepage.css';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="Homepage">
+        <header className="Homepage-header">
+          {/*<img src={logo} className="Homepage-logo" alt="logo" />*/}
+          <h1 className="Homepage-title">Caroline's Pages</h1>
+        </header>
+        <p className="Homepage-pages">
+            <a href="https://ceven.github.io/stamp-duty-calculator/">Stamp Duty Calculator</a>
+        </p>
+          <footer className="Homepage-footer">
+              <a href="https://github.com/ceven">My Github</a>
+          </footer>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
